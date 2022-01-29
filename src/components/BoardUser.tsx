6b773190e589251402070844b7c7b62ddf7ef25c
Lou-i3 <louis.rouault@gmail.com>
2022-01-29 19:26:25 +0100

    working login & register using REST API

---
+++ src/components/BoardUser.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+import { getUserBoard } from "../services/user.service";
+
+const BoardUser: React.FC = () => {
+  const [content, setContent] = useState<string>("");
+
+  useEffect(() => {
+    getUserBoard().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setContent(_content);
+      }
+    );
+  }, []);
+
+  return (
+    <div className="container">
+      <header className="jumbotron">
+        <h3>{content}</h3>
+      </header>
+    </div>
+  );
+};
+
+export default BoardUser;
