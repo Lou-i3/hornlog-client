788f4aeea171e4a5373f3a9642531a9857397abd
Lou-i3 <louis.rouault@gmail.com>
2022-02-06 19:53:58 +0100

    Some style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -44,11 +44,49 @@ const Nav = (props) => {
     };
 
     return (
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <Link to={"/"} className="navbar-brand">
-                HornLog
-            </Link>
-            <div className="navbar-nav mr-auto">
+        <nav className="navbar">
+            <div className="left-section">
+
+                <Link to={"/"} className="navbar-brand">
+                    HornLog
+                </Link>
+            </div>
+            <div className="center">
+
+            </div>
+
+            <div className="right-section">
+                {currentUser ? (
+                    <div className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link to={"/profile"} className="nav-link">
+                                {currentUser.username}
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/login" className="nav-link" onClick={logOut}>
+                                LogOut
+                            </a>
+                        </li>
+                    </div>
+                ) : (
+                    <div className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link to={"/login"} className="nav-link">
+                                Login
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to={"/register"} className="nav-link">
+                                Sign Up
+                            </Link>
+                        </li>
+                    </div>
+                )}
+            </div>
+
+            <div className="under-burger">
                 <li className="nav-item">
                     <Link to={"/home"} className="nav-link">
                         Home
@@ -96,34 +134,7 @@ const Nav = (props) => {
                 )}
             </div>
 
-            {currentUser ? (
-                <div className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link to={"/profile"} className="nav-link">
-                            {currentUser.username}
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <a href="/login" className="nav-link" onClick={logOut}>
-                            LogOut
-                        </a>
-                    </li>
-                </div>
-            ) : (
-                <div className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link to={"/login"} className="nav-link">
-                            Login
-                        </Link>
-                    </li>
 
-                    <li className="nav-item">
-                        <Link to={"/register"} className="nav-link">
-                            Sign Up
-                        </Link>
-                    </li>
-                </div>
-            )}
         </nav>
     );
 }
