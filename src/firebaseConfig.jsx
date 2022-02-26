76b9c200f9bf145d3c172e65634e3ac047033a99
Lou-i3 <louis.rouault@gmail.com>
2022-02-26 23:08:13 +0100

    oupsi

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,11 +1,11 @@
-// console.log("api key: ", window._env_.FIREBASE_API_KEY);
+// console.log("api key: ", window.__RUNTIME_CONFIG__.FIREBASE_API_KEY);
 const config = {
-    apiKey: window._env_.FIREBASE_API_KEY,
-    authDomain: window._env_.FIREBASE_AUTH_DOMAIN,
-    projectId: window._env_.FIREBASE_PROJECT_ID,
-    storageBucket: window._env_.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: window._env_.FIREBASE_MESSAGING_SENDER_ID,
-    appId: window._env_.FIREBASE_APP_ID,
-    measurementId: window._env_.FIREBASE_MESUREMENT_ID
+    apiKey: window.__RUNTIME_CONFIG__.FIREBASE_API_KEY,
+    authDomain: window.__RUNTIME_CONFIG__.FIREBASE_AUTH_DOMAIN,
+    projectId: window.__RUNTIME_CONFIG__.FIREBASE_PROJECT_ID,
+    storageBucket: window.__RUNTIME_CONFIG__.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: window.__RUNTIME_CONFIG__.FIREBASE_MESSAGING_SENDER_ID,
+    appId: window.__RUNTIME_CONFIG__.FIREBASE_APP_ID,
+    measurementId: window.__RUNTIME_CONFIG__.FIREBASE_MESUREMENT_ID
 };
 export default config;
