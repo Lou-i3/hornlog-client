0e4969ebdc9bfae8e90c26cd846dfdc3ca238119
Lou-i3 <louis.rouault@gmail.com>
2022-02-26 22:21:04 +0100

    support for env variables with docker

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,11 @@ RUN npm run build
 # production environment
 FROM nginx:stable-alpine
 COPY --from=build /app/build /usr/share/nginx/html
+
+
+COPY ./env.sh /usr/share/nginx/html
+RUN chmod +x env.sh
+
 # if using React Router, add the following line:
 # RUN rm -v /etc/nginx/nginx.conf
 
@@ -29,4 +34,4 @@ ENV FIREBASE_APP_ID aoo id
 ENV FIREBASE_MESUREMENT_ID id
 
 EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+CMD ["/bin/sh", "-c", "/usr/share/nginx/html/env.sh", "&&", "nginx", "-g", "daemon off;"]
--- src/App.tsx
+++ src/App.tsx
@@ -29,11 +29,13 @@ import Loading from './components/pages/Loading';
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+
 // require('dotenv').config({ path: "./.env" });
-console.log("api key (react app): ", process.env.REACT_APP_FIREBASE_API_KEY);
-console.log("api url: ", process.env.REACT_APP_API_URL);
-console.log("api url: ", process.env.API_URL);
-console.log("api key: ", process.env.FIREBASE_API_KEY);
+console.log("api key (react app): ", window._env_.REACT_APP_FIREBASE_API_KEY);
+console.log("api url: ", window._env_.REACT_APP_API_URL);
+console.log("api url: ", window._env_.API_URL);
+console.log("api key: ", window._env_.FIREBASE_API_KEY);
+console.log("api key: ", window._env_.FIREBASE_API_KEY);
 
 const firebaseAppAuth = firebaseApp.auth();
 
@@ -55,7 +57,7 @@ function OkQuery(_props: { user: any; }) {
     console.log(_props.user)
     console.log("user: " + _props.user.currentUser);
   }
-  console.log("api key: ", process.env.REACT_APP_FIREBASE_API_KEY);
+  console.log("api key: ", window._env_.REACT_APP_FIREBASE_API_KEY);
   return (
     <div className="okQuery">
       <p>OkQuery</p>
