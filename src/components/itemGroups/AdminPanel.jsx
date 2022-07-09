550f426f16f5482d04226d5352dc3e4f48942495
Lou-i3 <louis.rouault@gmail.com>
2022-07-09 17:33:30 +0200

    added icons link to admin settings

--- src/components/itemGroups/AdminPanel.jsx
+++ src/components/itemGroups/AdminPanel.jsx
@@ -2,21 +2,28 @@ import GendersEdit from "../items/settings/GendersEdit";
 import SettingsHeader from "../items/settings/SettingsHeader";
 import Icon from "../global/Icon";
 import AllUsers from "./allUsers";
+import { Link } from "react-router-dom";
 
 const AdminPanel = () => {
-    return ( 
-    <div className="settingsColumn">
-        <SettingsHeader icon="settings" title="Admin Panel" />
-        <button className="button inactive">
+    return (
+        <div className="settingsColumn">
+            <SettingsHeader icon="settings" title="Admin Panel" />
+            <button className="button inactive">
                 <Icon type="save" />
                 <span>Save</span>
             </button>
-        <div className="blocksWrapper">
-            <GendersEdit type="app" />
-            <AllUsers />
+            <div className="blocksWrapper">
+                <GendersEdit type="app" />
+                <AllUsers />
+                <Link className="settingsBlock icons" to="/icons">
+                    <div className="settingsBlockHeader icons">
+                        <Icon type="icons" />
+                        <h3>Icons</h3>
+                    </div>
+                </Link>
+            </div>
         </div>
-    </div> 
     );
 }
- 
+
 export default AdminPanel;
